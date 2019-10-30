93feb36b3e0e00d4bfea3a69ccb0f07443f74824
LXL <328161507@qq.com>
2019-10-30 09:36:23 +0800

    把一些配置提取到properties文件

--- src/main/java/life/lxl/community/community/controller/AuthorizeController.java
+++ src/main/java/life/lxl/community/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import life.lxl.community.community.dto.AccessTokenDTO;
 import life.lxl.community.community.dto.GithupUser;
 import life.lxl.community.community.provider.GithupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,15 +15,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GithupProvider githupProvider;
+    @Value("${githup.client.id}")
+    private String clientId;
+    @Value("${githup.client.secret}")
+    private String client_secret;
+    @Value("${githup.Redirect_uri}")
+    private String redirect_uri;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name  ="code") String code,
                            @RequestParam(name="state") String state){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("77e29ec87ed5c314b1e5");
-        accessTokenDTO.setClient_secret("0d0f793514d17edb1cb956d5b756e71b41f86112");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8080/callback");
+        accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = githupProvider.getAccessToken(accessTokenDTO);
         GithupUser user = githupProvider.getUser(accessToken);
