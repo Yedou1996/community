6364e1fc4181adebc42d9646defe0b49f1a66088
LXL <328161507@qq.com>
2019-11-05 09:29:25 +0800

    集成mybatis实现登录用户插入用户信息到mysql

--- src/main/java/life/lxl/community/community/controller/AuthorizeController.java
+++ src/main/java/life/lxl/community/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package life.lxl.community.community.controller;
 
 import life.lxl.community.community.dto.AccessTokenDTO;
 import life.lxl.community.community.dto.GithupUser;
+import life.lxl.community.community.mapper.UserMapper;
+import life.lxl.community.community.model.User;
 import life.lxl.community.community.provider.GithupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 
 @Controller
@@ -21,10 +25,13 @@ public class AuthorizeController {
     private String client_secret;
     @Value("${githup.Redirect_uri}")
     private String redirect_uri;
+   @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name  ="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(client_secret);
@@ -32,8 +39,19 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirect_uri);
         accessTokenDTO.setState(state);
         String accessToken = githupProvider.getAccessToken(accessTokenDTO);
-        GithupUser user = githupProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return  "index";
+        GithupUser githupUser = githupProvider.getUser(accessToken);
+       if (githupUser != null){
+           User user = new User();
+           user.setToken(UUID.randomUUID().toString());
+           user.setAccountId(String.valueOf(githupUser.getId()));
+           user.setName(githupUser.getName());
+           user.setGmtCreate(System.currentTimeMillis());
+           user.setGmtModified(user.getGmtCreate());
+           userMapper.insert(user);
+           request.getSession().setAttribute("user",githupUser);
+           return "redirect:/";
+       }else {
+           return "redirect:/";
+       }
     }
 }
